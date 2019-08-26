153e2ab88c3e06033061148c5e14dcfdd87d3bf4
Jack <rainy_waltz@naver.com>
2019-08-27 01:23:58 +0900

    jsx 문법에서의 조건문

--- React/4. Working with Lists and Conditionals/my-react-app/src/App.js
+++ React/4. Working with Lists and Conditionals/my-react-app/src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Jay', age: 23 }
+    ],
+    showPersons: false
+  };
+
+  switchNameHandler = newname => {
+    this.setState({
+      persons: [
+        { name: newname, age: 1 },
+        { name: 'asdfg', age: 123 },
+        { name: 'TTffT', age: 112 }
+      ]
+    });
+  };
+
+  nameChangedHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'KKK', age: 1 },
+        { name: event.target.value, age: 21 },
+        { name: 'TTT', age: 31 }
+      ]
+    });
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  };
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className='App'>
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.togglePersonHandler} style={style}>
+          Switch Name
+        </button>
+
+        {this.state.showPersons ? (
+          <div>
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              changed={this.nameChangedHandler}
+            />
+            <Person
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              changed={this.nameChangedHandler}
+            >
+              MY hobby
+            </Person>
+            <Person
+              name={this.state.persons[2].name}
+              age={this.state.persons[2].age}
+            />
+          </div>
+        ) : null}
+
+        <h1>Hi</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
